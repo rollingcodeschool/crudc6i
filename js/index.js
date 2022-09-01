0e35e7fe12ce7648560d6fb5b521340dc8f85cd8
Emilse Arias <emilsearias@gmail.com>
2022-09-01 18:28:33 -0300

    redireccionamos a la web de detalle cuando selecionamos el boton de la card

--- js/index.js
+++ js/index.js
@@ -12,9 +12,15 @@ function crearColumna(pelicula){
       <img src="${pelicula.imagen}" class="card-img-top" alt="${pelicula.titulo}">
       <div class="card-body">
         <h5 class="card-title">${pelicula.titulo}</h5>
-        <button class="btn btn-primary">ver detalle</button>
+        <button class="btn btn-primary" onclick="detallePelicula('${pelicula.codigo}')">ver detalle</button>
       </div>
     </div>
   </aside>
     `
+}
+
+function detallePelicula(codigo){
+    console.log(codigo)
+    console.log(window.location.origin + '/pages/detalle.html?codigo='+codigo);
+    window.location.href = window.location.origin + '/pages/detalle.html?codigo='+codigo;
 }
